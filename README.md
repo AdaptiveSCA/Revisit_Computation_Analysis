70ab8012840a26c699e7a7f50caaaee776c87f01
AdaptiveSCA <1243771808@qq.com>
2023-02-24 16:50:43 +0800

    initialized at github

---
+++ README.md
@@ -0,0 +1,20 @@
+# Revisit Computation Analysis
+
+This repository contains the supporting code for the experiment of computation analysis on different internal encodings.
+
+The attack results are recorded in "xxx_distinguisher.txt".
+
+## Build
+
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
+
+## Run
+
+```
+$ ./ENCA
+```
---
+++ test/main.c
@@ -0,0 +1,135 @@
+#include "ENCA.h"
+
+int main()
+{
+    uint8_t map[total][256];
+    FILE *fp;
+    int count;
+    for(int Case = 1; Case <= 14; Case++)
+    {
+        for(int Test = 0; Test < total; Test++)
+        {
+            generate_map(map[Test], Case);
+        }
+
+        //-----DCA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(DCA(map[Test])) count++;
+        }
+        printf("DCA #%d broken tests: %d\n", Case, count);
+        fp = fopen("DCA_distinguisher.txt", "a");
+        fprintf(fp, "DCA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+
+        //-----IDCA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(IDCA(map[Test])) count++;
+        }
+        printf("IDCA #%d broken tests: %d\n", Case, count);
+        fp = fopen("IDCA_distinguisher.txt", "a");
+        fprintf(fp, "IDCA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+
+        //-----CPA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(CPA(map[Test])) count++;
+        }
+        printf("CPA #%d broken tests: %d\n", Case, count);
+        fp = fopen("CPA_distinguisher.txt", "a");
+        fprintf(fp, "CPA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+
+        // //-----CA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(CA(map[Test])) count++;
+        }
+        printf("CA #%d broken tests: %d\n", Case, count);
+        fp = fopen("CA_distinguisher.txt", "a");
+        fprintf(fp, "CA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+
+        //-----MIA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(MIA(map[Test])) count++;
+        }
+        printf("MIA #%d broken tests: %d\n", Case, count);
+        fp = fopen("MIA_distinguisher.txt", "a");
+        fprintf(fp, "MIA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+        
+        //-----BCA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if(BCA(map[Test])) count++;
+        }
+        printf("BCA #%d broken tests: %d\n", Case, count);
+        fp = fopen("BCA_distinguisher.txt", "a");
+        fprintf(fp, "BCA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+
+        //-----SA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(SA(map[Test])) count++;
+        }
+        printf("SA #%d broken tests: %d\n", Case, count);
+        fp = fopen("SA_distinguisher.txt", "a");
+        fprintf(fp, "SA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+      
+        //-----MSA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(MSA(map[Test])) count++;
+        }
+        printf("MSA #%d broken tests: %d\n", Case, count);
+        fp = fopen("MSA_distinguisher.txt", "a");
+        fprintf(fp, "MSA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+        
+        //-----ISA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(ISA(map[Test])) count++;
+        }
+        printf("ISA #%d broken tests: %d\n", Case, count);
+        fp = fopen("ISA_distinguisher.txt", "a");
+        fprintf(fp, "ISA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+        
+        //-----ADCA-----
+        count = 0; 
+        for(int Test = 0; Test < total; Test++)
+        {
+            if((((Test + 1) % (int)(total * 0.2)) == 0) && (Test < total - 1)) printf("processing: %d\%\n", (Test + 1) * 100 / total);
+            if(ADCA(map[Test])) count++;
+        }
+        printf("ADCA #%d broken tests: %d\n", Case, count);
+        fp = fopen("ADCA_distinguisher.txt", "a");
+        fprintf(fp, "ADCA #%d broken tests: %d\n", Case, count);
+        fclose(fp);
+    }
+    return 0;
+}
